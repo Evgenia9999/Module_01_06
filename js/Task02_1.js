840b932b68e65a1a4b410db0bb699c5de89918d2
Evgenia Avramenko <hapjane@yandex.ru>
2023-07-21 10:55:00 +0500

    Переделала 2 задание

--- js/Task02_1.js
+++ js/Task02_1.js
@@ -1,31 +1,90 @@
-let numCheck = +prompt('Введите положительное число');
+const isPrime = (num) => {
 
-let num = numCheck;
-
-let a = false;
-
-if (num === 1) {
-    console.log('false');
+if (num <= 1) {
+    console.log(false);
 
 } else if (num === 2) {
-        console.log('true');
-        
-
+    console.log(true);
+    
 } else if (num > 2) {
 
-    for (let i = 2; i < numCheck; i++) {
+    for (let i = 2; i < num; i++) {
 
-    a = (num % i === 0);
-
-    console.log(i);
-
-    if (a === true) {
-        console.log('false');
-        break;
-    } 
-} 
-
-if (a === false) {
-    console.log('true');
-}  
+        console.log(i);
+    
+        if (!(num % i)) {
+            const answ = false;
+            console.log(false);
+            break;
+        } else if (i === (num - 1)) {
+    
+            console.log(true);
+        }
+    }
 }
+// return num > 1;
+
+}
+
+isPrime();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numCheck = +prompt('Введите положительное число');
+
+// let num = numCheck;
+
+// let a = false;
+
+// if (num === 1) {
+//     console.log('false');
+
+// } else if (num === 2) {
+//         console.log('true');
+        
+
+// } else if (num > 2) {
+
+//     for (let i = 2; i < numCheck; i++) {
+
+//     a = (num % i === 0);
+
+//     console.log(i);
+
+//     if (a === true) {
+//         console.log('false');
+//         break;
+//     } 
+// } 
+
+// if (a === false) {
+//     console.log('true');
+// }  
+// }
