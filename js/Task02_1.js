03082e7acb4058f42e5e70cb11c1fa52920c3dc4
Evgenia Avramenko <hapjane@yandex.ru>
2023-07-22 23:06:04 +0500

    Обновила Task02_01

--- js/Task02_1.js
+++ js/Task02_1.js
@@ -26,7 +26,21 @@ if (num <= 1) {
 
 }
 
-isPrime();
+isPrime(5);
+
+
+
+
+// const isPrime = (num) => {
+//     for (let i = 2; i < num; i++) {
+//     if (num % i === 0) {
+//     return false;
+//     }
+//     }
+//     return num > 1;
+//     };
+    
+//     console.log(isPrime(2))
 
 
 
